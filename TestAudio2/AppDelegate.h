4c7d4e57ef3948b0b709495ab97d4090fc5b942b
gtchen66 <gtchen66@yahoo.com>
2014-03-05 13:28:35 -0800

    Initial Commit

---
+++ TestAudio2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestAudio2
+//
+//  Created by George Chen on 3/5/14.
+//  Copyright (c) 2014 George Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
